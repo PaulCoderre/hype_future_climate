bd8a76187e5b2f95bb98d0e214ce330325702bb7
PaulCoderre <paulcoderre6@gmail.com>
2025-03-31 10:38:01 -0600

    initial commit

---
+++ scripts/.ipynb_checkpoints/run_individual_member-checkpoint.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=8
+#SBATCH --time=0-3:00:00
+#SBATCH --mem=96G
+#SBATCH --job-name=scenarios
+#SBATCH --error=slurm_logs/slurm_%j.err
+#SBATCH --output=slurm_logs/slurm_%j.out
+
+# Add the directory where the modules are located to the MODULEPATH
+. /work/comphyd_lab/local/modules/spack/2024v5/lmod-init-bash
+module unuse $MODULEPATH
+module use /work/comphyd_lab/local/modules/spack/2024v5/modules/linux-rocky8-x86_64/Core/
+
+module restore scimods
+source ~/virtual-envs/scienv/bin/activate
+
+
+python run_individual_member.py
+
+
+
+
+
